d1d4728cafd75da7a98fce64132be2ec31de7188
Tamotsu Takahashi <ttakah+github@gmail.com>
2023-08-13 22:04:10 +0900

    Rearrange cypress.spec.js

--- cypress.spec.js
+++ cypress.spec.js
@@ -1,19 +1,15 @@
 describe('test extsub', () => {
-  it('visit the page', () => {
-    cy.visit('https://tamo.github.io/extsub/')
-  })
   it('checks everything is loaded', () => {
     cy.on('uncaught:exception', (err, runnable, promise) => {
       expect(err.message).to.contain('SharedArrayBuffer')
       done()
       return false
     })
+    cy.visit('https://tamo.github.io/extsub/')
     cy.get('#logs[data-loaded="true"]', { timeout: 20000 })
   })
-  it('upload mp4', () => {
+  it('uploads a file', () => {
     cy.get('input#uploader').selectFile('test.mp4')
-  })
-  it('result', () => {
     cy.get('#subtext', { timeout: 10000 })
       .should('contain', 'タイトル ()')
       .and('contain', 'この動画は')
