0980b74721b1d9e3d0c3377a1b5d32ea25a6e932
Tamotsu Takahashi <ttakah+github@gmail.com>
2023-08-13 21:24:51 +0900

    Configure e2e

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   experimentalWebKitSupport: true,
-  },
+  e2e: {
+    supportFile: false,
+    specPattern: "cypress.spec.js"
+  }
+},
 )
