b906c35bfc571a784231b5290151c74d8e38c921
Tamotsu Takahashi <ttakah+github@gmail.com>
2023-08-13 22:05:02 +0900

    Disable testIsolation

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   experimentalWebKitSupport: true,
   e2e: {
+    testIsolation: false,
     supportFile: false,
     specPattern: "cypress.spec.js"
   }
